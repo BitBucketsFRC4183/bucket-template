5b2f2a55714d5b324fd53b2ba74e3575fe2894c0
matt <yuuki@superyuuki.com>
2023-01-13 15:55:26 -0700

    fix timezone

--- src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
+++ src/main/java/org/bitbuckets/bootstrap/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "season-template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "419abcef1ee4ca01702b64a846aafba3581fca3d";
-  public static final String GIT_DATE = "2023-01-12 18:24:23 EST";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "45af5551f4b86cfa75d7dcab8c40e97e2c61532e";
+  public static final String GIT_DATE = "2023-01-13 17:46:01 EST";
   public static final String GIT_BRANCH = "matt_nukes_the_code";
-  public static final String BUILD_DATE = "2023-01-13 08:13:33 EST";
-  public static final long BUILD_UNIX_TIME = 1673615613441L;
+  public static final String BUILD_DATE = "2023-01-13 17:47:05 EST";
+  public static final long BUILD_UNIX_TIME = 1673650025073L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/org/bitbuckets/lib/motor/setpoint/TuneableTalonMotor.java
+++ src/main/java/org/bitbuckets/lib/motor/setpoint/TuneableTalonMotor.java
@@ -1,8 +1,15 @@
 package org.bitbuckets.lib.motor.setpoint;
 
+import java.util.Optional;
+
 public class TuneableTalonMotor implements IMotor{
     @Override
     public void moveAt(double units) {
 
+    }
+
+    @Override
+    public <T> Optional<T> underlyingMotor(Class<T> typeOfMotor) {
+        return Optional.empty();
     }
 }
